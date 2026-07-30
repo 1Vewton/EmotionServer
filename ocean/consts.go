88653cc15d730ca452c9e3dcfe253d444a5ccf71
NewtonZhan <zhanyunze0601@gmail.com>
2026-07-31 02:44:19 +0800

    feat(const): Add the processing matrix for the initial emotion

---
+++ ocean/consts.go
@@ -0,0 +1,56 @@
+package ocean
+
+// GetOceanLineOne gets the line 1 for processing matrix of the initial emotion
+func GetOceanLineOne() []float64 {
+	return []float64{
+		0.0,
+		0.0,
+		0.30,
+		0.30,
+		-0.10,
+	}
+}
+
+// GetOceanLineTwo gets the line 2 for processing matrix of the initial emotion
+func GetOceanLineTwo() []float64 {
+	return []float64{
+		0.20,
+		0.0,
+		0.30,
+		0.0,
+		-0.30,
+	}
+}
+
+// GetOceanLineThree gets the line 3 for processing matrix of the initial emotion
+func GetOceanLineThree() []float64 {
+	return []float64{
+		0.0,
+		0.20,
+		0.30,
+		-0.20,
+		0.0,
+	}
+}
+
+// GetOceanLineFour gets the line 4 for processing matrix of the initial emotion
+func GetOceanLineFour() []float64 {
+	return []float64{
+		0.0,
+		0.30,
+		0.0,
+		0.20,
+		-0.10,
+	}
+}
+
+// GetOceanLineFive gets the line 5 for processing matrix of the initial emotion
+func GetOceanLineFive() []float64 {
+	return []float64{
+		0.4,
+		-0.2,
+		0.0,
+		0.0,
+		0.0,
+	}
+}
