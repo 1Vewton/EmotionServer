22e3914ba5d653c2a0c2816ba1a73779c3583a32
NewtonZhan <zhanyunze0601@gmail.com>
2026-08-06 00:16:26 +0800

    feat(emotion): Add processing of initial emotion

--- ocean/ocean.go
+++ ocean/ocean.go
@@ -2,5 +2,9 @@ package ocean
 
 // Personality stores the OCEAN personality
 type Personality struct {
-	Openness float64
+	Openness          float64
+	Conscientiousness float64
+	Extraversion      float64
+	Agreeableness     float64
+	Neuroticism       float64
 }
---
+++ ocean/processocean/processocean.go
@@ -0,0 +1,51 @@
+package processocean
+
+import (
+	"github.com/1Vewton/EmotionServer/ocean"
+	"github.com/1Vewton/EmotionServer/utils/mathematics"
+)
+
+// GetPleasure gets the pleasure of the initial emotion.
+func GetPleasure(personality ocean.Personality) float64 {
+	processMatirx := ocean.GetOceanLineOne()
+	return mathematics.DotProductProcessing(
+		personality,
+		[5]float64(processMatirx),
+	)
+}
+
+// GetArousal gets the arousal of the initial emotion.
+func GetArousal(personality ocean.Personality) float64 {
+	processMatirx := ocean.GetOceanLineTwo()
+	return mathematics.DotProductProcessing(
+		personality,
+		[5]float64(processMatirx),
+	)
+}
+
+// GetDominance gets the dominance of the initial emotion.
+func GetDominance(personality ocean.Personality) float64 {
+	processMatirx := ocean.GetOceanLineThree()
+	return mathematics.DotProductProcessing(
+		personality,
+		[5]float64(processMatirx),
+	)
+}
+
+// GetCertainty gets the certainty of the initial emotion
+func GetCertainty(personality ocean.Personality) float64 {
+	processMatirx := ocean.GetOceanLineFour()
+	return mathematics.DotProductProcessing(
+		personality,
+		[5]float64(processMatirx),
+	)
+}
+
+// GetNovelty gets the novelty of the initial emotion
+func GetNovelty(personality ocean.Personality) float64 {
+	processMatirx := ocean.GetOceanLineFive()
+	return mathematics.DotProductProcessing(
+		personality,
+		[5]float64(processMatirx),
+	)
+}
